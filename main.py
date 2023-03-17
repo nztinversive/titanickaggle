2e4c6d1081eb96d7ad34f4190d5973e953ea6084
Noah Thies <thiesnoah@hotmail.com>
2023-03-16 19:00:34 -0500

    Added Grid search

--- main.py
+++ main.py
@@ -1,31 +1,53 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import GridSearchCV
 
-# Load data
-data = pd.read_csv('train.csv')
+# Load the dataset
+data = pd.read_csv("train.csv")
 
-# Preprocess data
-def preprocess_data(df):
-    df = df.drop(['Name', 'Ticket', 'Cabin'], axis=1)
-    df['Age'] = df['Age'].fillna(df['Age'].mean())
-    df['Embarked'] = df['Embarked'].fillna(df['Embarked'].mode()[0])
-    df = pd.get_dummies(df, columns=['Sex', 'Embarked'], drop_first=True)
-    return df
+# Preprocessing
+data['Age'].fillna(data['Age'].median(), inplace=True)
+data['Embarked'].fillna(data['Embarked'].mode()[0], inplace=True)
+data['FamilySize'] = data['SibSp'] + data['Parch'] + 1
+data['IsAlone'] = 1
+data['IsAlone'].loc[data['FamilySize'] > 1] = 0
 
-data = preprocess_data(data)
+data = data.drop(['Cabin', 'Ticket', 'Name', 'PassengerId', 'SibSp', 'Parch'], axis=1)
+data = pd.get_dummies(data, columns=['Sex', 'Embarked'])
 
-# Split data into training and validation sets
-X = data.drop('Survived', axis=1)
-y = data['Survived']
-X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
+# Define features and target
+X = data.drop("Survived", axis=1)
+y = data["Survived"]
 
-# Train a RandomForestClassifier
-model = RandomForestClassifier(n_estimators=100, random_state=42)
+# Split the dataset
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Standardize the data
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+# Hyperparameter tuning with GridSearchCV
+param_grid = {
+    'n_estimators': [100, 200, 300],
+    'max_depth': [None, 10, 20, 30],
+    'min_samples_split': [2, 5, 10],
+    'min_samples_leaf': [1, 2, 4],
+}
+
+grid_search = GridSearchCV(estimator=RandomForestClassifier(random_state=42), param_grid=param_grid, cv=5)
+grid_search.fit(X_train, y_train)
+best_params = grid_search.best_params_
+print("Best parameters:", best_params)
+
+# Train the model with the best parameters
+model = RandomForestClassifier(**best_params, random_state=42)
 model.fit(X_train, y_train)
 
-# Validate the model
-y_pred = model.predict(X_val)
-accuracy = accuracy_score(y_val, y_pred)
-print(f'Accuracy: {accuracy:.4f}')
+# Make predictions and calculate the accuracy
+y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
